c9204a5b7b06557e33efe1af6b8865d3634ff244
alexandre025 <alexandre.ferraille@gmail.com>
2017-10-03 21:14:43 +0200

    root to: /admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount Cdx::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'cdx/admin/home#index'
+
   post 'calls/handle', to: 'calls#handle'
   post 'calls/action', to: 'calls#action'
   post 'calls/complete', to: 'calls#complete'
